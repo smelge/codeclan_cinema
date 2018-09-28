031519301033b60dc80bdd54ece46cd007a062af
smelge <traildogfilms@gmail.com>
2018-09-28 10:57:47 +0100

    Set up database and tables

---
+++ db/cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS films;
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT8
+);
+
+CREATE TABLE films (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT2
+);
+
+CREATE TABLE tickets (
+  id SERIAL8 PRIMARY KEY,
+  customer_id INT2 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT2 REFERENCES films(id) ON DELETE CASCADE
+);
